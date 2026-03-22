d6fb71b4a6c969a1007fb5083021987654219e35
lailachoud <lc3957@nyu.edu>
2026-03-21 20:37:34 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -44,7 +44,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = b'Tandon' # Remember it should be a byte-object
-password = lc3957@nyu.edu
+password = 'lc3957@nyu.edu'
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
