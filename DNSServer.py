ee75f58a4f18ebbbf652cde7913fa61e3558de4a
lailachoud <lc3957@nyu.edu>
2026-03-21 20:35:59 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -35,6 +35,7 @@ def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     encrypted_data = f.encrypt(input_string.encode('utf-8')) #call the Fernet encrypt method  
+    return encrypted_data
 
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
@@ -112,7 +113,7 @@ def run_dns_server():
             response = dns.message.make_response(request)
 
             # Get the question from the request
-            question = request.question[???]
+            question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
 
